5ff7da2a418c8231fdc7f952a94b8619ab9f8fb4
NghiaTV3 <trannghi170735@gmail.com>
2026-04-08 08:26:22 +0700

    20.Prescription API

---
+++ Appointment/src/main/java/com/hms/appointment/entity/Prescription.java
@@ -0,0 +1,36 @@
+package com.hms.appointment.entity;
+
+
+import com.hms.appointment.dto.PrescriptionDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Prescription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long patientId;
+    private Long doctorId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appointment_id")
+    private Appointment appointment;
+    private LocalDate prescriptionDate;
+    private String notes;
+
+    public Prescription(Long id){
+        this.id = id;
+    }
+
+    public PrescriptionDTO toDTO() {
+        return new PrescriptionDTO(this.id, this.patientId, this.doctorId, appointment.getId(),
+                this.prescriptionDate, this.notes, null);
+    }
+}
---
+++ Appointment/src/main/java/com/hms/appointment/service/PrescriptionService.java
@@ -0,0 +1,13 @@
+package com.hms.appointment.service;
+
+import com.hms.appointment.dto.PrescriptionDTO;
+import com.hms.appointment.exception.HmsException;
+
+public interface PrescriptionService {
+
+    public Long savePrescription(PrescriptionDTO request) throws HmsException;
+
+    public PrescriptionDTO getPrescriptionByAppointmentId(Long appointmentId) throws HmsException;
+
+    public PrescriptionDTO getPrescriptionById(Long prescriptionId) throws HmsException;
+}
